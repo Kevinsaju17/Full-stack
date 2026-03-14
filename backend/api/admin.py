eef50c9a1a2fe5d8514aa819f522d258a65d436b
kevinsaju17 <kevinsaju003@gmail.com>
2026-03-14 11:12:56 +0530

    6.1

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import*
+
+
+admin.site.register(Country)
+admin.site.register(League)
+admin.site.register(Characteristic)
+admin.site.register(Footballclub)   
+
 # Register your models here.
--- backend/crud/settings.py
+++ backend/crud/settings.py
@@ -39,12 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'api'
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -53,7 +54,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173/',
+    'http://localhost:5173',
 ]
 
 ROOT_URLCONF = 'crud.urls'
