c9a2c9782a07c8ab3f75293fe65693bbe7cb505a
Kevin Saju <kevinsaju003@gmail.com>
2026-03-19 20:54:06 +0530

    7

--- frontend/src/components/Create.jsx
+++ frontend/src/components/Create.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { React, useState, useEffect } from "react"
+import AxiosInstance from "./Axios"
 
-const Create =() =>{
-    return(
+const Create = () => {
+
+    const [country, setCountry] = useState([])
+    const [league, setLeague] = useState([])
+    const [charateristic, setcharateristic] = useState([])
+
+
+    console.log("Country", country)
+    console.log("League", league)
+    console.log("Characteristic", charateristic)
+
+    const GetData = () => {
+        AxiosInstance.get('country/').then((res) => {
+            setCountry(res.data)
+
+        })
+
+        AxiosInstance.get('league/').then((res) => {
+            setLeague(res.data)
+
+        })
+
+        AxiosInstance.get('characteristic/').then((res) => {
+            setcharateristic(res.data)
+
+        })
+    }
+    useEffect(() => {
+        GetData()
+    }, [])
+    return (
         <div>
             this is Create page
         </div>
